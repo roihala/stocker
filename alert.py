29ec568935650f0bbf492a9da78cacf9b49525d6
Roi Halamish <roihala@hotmail.com>
2020-10-08 22:44:37 +0300

    fix

--- alert.py
+++ alert.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import argparse
 import logging
 import os
@@ -23,13 +21,7 @@ def main():
     args = get_args()
     logging.basicConfig(filename=LOGGER_PATH, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
-    tickers_file = args.tickers
-    
-    if not tickers_file:
-        tickers_file = DEFAULT_CSV_PATH
-
-    tickers = extract_tickers(tickers_file)
-    alert_tickers(tickers, args.debug)
+    alert_tickers(args, args.debug)
 
 
 def alert_tickers(args, debug=False):
@@ -49,7 +41,7 @@ def alert_tickers(args, debug=False):
             next_hour = next_hour.shift(hours=1)
             pause.until(next_hour.timestamp)
 
-        for ticker in args.tickers_list:
+        for ticker in extract_tickers(args):
             try:
                 with TickerHistory(ticker, mongo_db) as ticker_history:
                     if debug:
@@ -65,9 +57,9 @@ def alert_tickers(args, debug=False):
             break
 
 
-def extract_tickers(csv_path):
+def extract_tickers(args):
     try:
-        df = pandas.read_csv(csv_path)
+        df = pandas.read_csv(args.csv)
         return df.Symbol.apply(lambda ticker: ticker.upper())
     except Exception:
         raise Exception('Invalid csv file - validate the path and that the tickers are under a column named symbol')
@@ -75,7 +67,7 @@ def extract_tickers(csv_path):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--tickers_file', dest='tickers', help='path to custom csv-format tickers file')
+    parser.add_argument('--csv', dest='csv', help='path to csv tickers file', required=True)
     parser.add_argument('--change', dest='change', help='Whether a changed occur in the ticker parameters', default='')
     parser.add_argument('--debug', dest='debug', help='debug_mode', default=False, action='store_true')
     parser.add_argument('--user', dest='user', help='username for MongoDB')
