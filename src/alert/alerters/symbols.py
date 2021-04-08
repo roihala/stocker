e512b9b4c272d2aaff9e7239dce574bdd7ac2e37
Roi Halamish <roihala@hotmail.com>
2021-04-08 21:25:48 +0300

    Alerting boolean alerts differently now

--- src/alert/alerters/symbols.py
+++ src/alert/alerters/symbols.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from src.alert.alerter_base import AlerterBase
 from src.alert.alerters import Profile
 
@@ -9,7 +11,7 @@ class Symbols(AlerterBase):
 
     @property
     def hierarchy(self):
-        return {'verifiedProfile':  [False, True],
+        return {'verifiedProfile': [False, True],
                 'isHotSector': [False, True],
                 'hasPromotion': [False, True],
                 'transferAgentVerified': [False, True],
@@ -22,11 +24,34 @@ class Symbols(AlerterBase):
                 'isBankrupt': [True, False],
                 'hasControlDispute': [True, False]}
 
+    def get_alert_msg(self, diff):
+        original_diff = deepcopy(diff)
+        diff = self._edit_diff(diff)
+
+        if not diff:
+            return ''
+
+        msg = None
+
+        if diff.get('diff_type') == 'change':
+            if diff.get('old') is False:
+                msg = '{green_circle_emoji}{key} added'.format(green_circle_emoji=self.GREEN_CIRCLE_EMOJI_UNICODE,
+                                                               key=diff.get('changed_key'))
+            elif diff.get('old') is True:
+                msg = '{red_circle_emoji}{key} added'.format(red_circle_emoji=self.RED_CIRCLE_EMOJI_UNICODE,
+                                                             key=diff.get('changed_key'))
+
+        # If managed to generate boolean-type alert message
+        if msg:
+            return msg
+        else:
+            # Falling back to regular format
+            super().get_alert_msg(original_diff)
+
     def _edit_diff(self, diff):
         diff = super()._edit_diff(diff)
 
         if diff and diff.get('changed_key') in Profile.OTCIQ_KEYS:
-
             diff = Profile.update_otciq(self._mongo_db, diff)
 
         return diff
