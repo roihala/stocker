af58618326f31cc3b2e72ac90286e39f57032ae8
Roi Halamish <roihala@hotmail.com>
2021-05-22 13:40:34 +0300

    Fix

--- src/read/reader_base.py
+++ src/read/reader_base.py
@@ -86,7 +86,7 @@ class ReaderBase(ABC):
         return history
 
     def get_entry_by_date(self, date, prev=False):
-        idx = self._sorted_history.index[self._sorted_history['date'] == date].tolist()
+        idx = self._sorted_history.index[self.get_sorted_history()['date'] == date].tolist()
         if len(idx) != 1:
             raise AttributeError("There should be a single index for each date")
 
