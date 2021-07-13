263bc9d6160fafa9c62414b68b08fefcce2d194c
Yoni Solimanov <rtyu28@gmail.com>
2021-07-13 10:40:04 +0300

    Changed scheduler infrastructure, and made collector with more replicas

--- collector_scheduler.py
+++ collector_scheduler.py
@@ -1,10 +1,6 @@
 # !/usr/bin/env python3
 import logging
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.triggers.combining import OrTrigger
-from apscheduler.triggers.date import DateTrigger
-from apscheduler.triggers.interval import IntervalTrigger
 from google.cloud import pubsub_v1
 
 from lighweight_runnable import LightRunnable
@@ -20,23 +16,10 @@ class CollectScheduler(LightRunnable):
         self.topic_name = self.PUBSUB_TICKERS_TOPIC_NAME + '-dev' if self._debug else self.PUBSUB_TICKERS_TOPIC_NAME
 
     def run(self):
-        scheduler = BlockingScheduler()
-
-        self.disable_apscheduler_logs()
-
-        trigger = OrTrigger([IntervalTrigger(minutes=5), DateTrigger()])
-
-        scheduler.add_job(self.publish_tickers,
-                          trigger=trigger,
-                          max_instances=1,
-                          misfire_grace_time=120)
-
-        scheduler.start()
-
-    def publish_tickers(self):
         self.logger.info("Publishing tickers")
         for ticker in self._tickers_list:
             self.publisher.publish(self.topic_name, ticker.encode('utf-8'))
+        self.logger.info("Finished publishing tickers")
 
 
 def main():
