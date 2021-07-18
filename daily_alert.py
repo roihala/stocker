b9d077d0ec158bc908eb04dd88d838f742cc28b9
Roi Halamish <roihala@hotmail.com>
2021-07-18 13:49:10 +0300

    .

--- daily_alert.py
+++ daily_alert.py
@@ -21,37 +21,37 @@ Check our website for pricing plans and more info."""
         self.remind_users()
 
     def remind_users(self):
-        # for user in self._mongo_db.telegram_users.find({'activation': 'trial'}):
-        #     try:
-        #         if 'trial_date' not in user:
-        #             self.logger.info(f"Updating {user.get('user_name')} trial date")
-        #             self._mongo_db.telegram_users.update_one(user, {'$set': {'trial_date': arrow.utcnow().format()}})
-        #             continue
-        #
-        #         # Capping trial date
-        #         trial_date = arrow.get(user['trial_date']) if arrow.get(user['trial_date']) > arrow.get(
-        #             '2021-06-08T13:00:00+00:00') else arrow.get('2021-06-08T13:00:00+00:00')
-        #         current_date = arrow.utcnow().floor('hours')
-        #
-        #         try:
-        #             trial_period = user['appendix']['weeks'] * 7
-        #         except KeyError:
-        #             trial_period = 14
-        #
-        #         remain = trial_period - (current_date - trial_date).days
-        #
-        #         if remain in [0, 2, 7] or remain < 0:
-        #             if remain <= 0:
-        #                 self.logger.info(f"{user.get('user_name')} trial ended")
-        #                 self._mongo_db.telegram_users.update_one(user, {'$set': {'activation': 'unregister'}})
-        #             msg = self.REMINDER_MESSAGE.format(punch_emoji=self.PUNCH_EMOJI_UNICODE,
-        #                                                formatted_time=self.get_formatted_time(remain))
-        #
-        #             self._telegram_bot.send_message(chat_id=user.get('chat_id'), text=msg,
-        #                                             reply_markup=self.SUBSCRIBE_KEYBOARD)
-        #     except Exception as e:
-        #         self.logger.warning(f"Couldn't remind user {user}")
-        #         self.logger.error(e)
+        for user in self._mongo_db.telegram_users.find({'activation': 'trial'}):
+            try:
+                if 'trial_date' not in user:
+                    self.logger.info(f"Updating {user.get('user_name')} trial date")
+                    self._mongo_db.telegram_users.update_one(user, {'$set': {'trial_date': arrow.utcnow().format()}})
+                    continue
+
+                # Capping trial date
+                trial_date = arrow.get(user['trial_date']) if arrow.get(user['trial_date']) > arrow.get(
+                    '2021-06-08T13:00:00+00:00') else arrow.get('2021-06-08T13:00:00+00:00')
+                current_date = arrow.utcnow().floor('hours')
+
+                try:
+                    trial_period = user['appendix']['weeks'] * 7
+                except KeyError:
+                    trial_period = 14
+
+                remain = trial_period - (current_date - trial_date).days
+
+                if remain in [0, 2, 7] or remain < 0:
+                    if remain <= 0:
+                        self.logger.info(f"{user.get('user_name')} trial ended")
+                        self._mongo_db.telegram_users.update_one(user, {'$set': {'activation': 'unregister'}})
+                    msg = self.REMINDER_MESSAGE.format(punch_emoji=self.PUNCH_EMOJI_UNICODE,
+                                                       formatted_time=self.get_formatted_time(remain))
+
+                    self._telegram_bot.send_message(chat_id=user.get('chat_id'), text=msg,
+                                                    reply_markup=self.SUBSCRIBE_KEYBOARD)
+            except Exception as e:
+                self.logger.warning(f"Couldn't remind user {user}")
+                self.logger.error(e)
 
         for user in self._mongo_db.telegram_users.find({'cancel_at': {'$exists': True}}):
             try:
