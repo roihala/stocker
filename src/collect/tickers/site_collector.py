14130df093514979db36e60826906284cd37a4a7
Yoni Solimanov <rtyu28@gmail.com>
2021-07-20 12:25:59 +0300

    Changed site collector to the new proxy

--- src/collect/tickers/site_collector.py
+++ src/collect/tickers/site_collector.py
@@ -1,18 +1,35 @@
 import logging
-import requests
+import random
 from abc import ABC, abstractmethod
 from copy import deepcopy
 from json import JSONDecodeError
+from urllib.error import URLError
 
+import requests
+import simplejson
 from requests import ReadTimeout
+from requests.auth import HTTPProxyAuth
 from retry import retry
-from urllib3.exceptions import MaxRetryError
+from urllib3.exceptions import MaxRetryError, SSLError, NewConnectionError
 
-from runnable import Runnable
 from src.collect.tickers.ticker_collector import TickerCollector
 from src.find.site import InvalidTickerExcpetion
 
 logger = logging.getLogger('Collect')
+proxy = "http://zproxy.lum-superproxy.io:22225"
+
+ips = []
+
+
+@retry((JSONDecodeError, simplejson.errors.JSONDecodeError, ReadTimeout), tries=12, delay=0.25)
+def get_ips():
+    res = requests.get("https://brightdata.com/api/zone/ips?zone=data_center",
+                       headers={"Authorization": "Bearer 9a8bc5df8b0e14bc21f1ca755f37714d"})
+    return res.json()['ips']
+
+
+ips = get_ips()
+
 
 class SiteCollector(TickerCollector, ABC):
     @property
@@ -20,8 +37,23 @@ class SiteCollector(TickerCollector, ABC):
     def site(self):
         pass
 
+    def get_proxy_auth(self):
+        username = 'lum-customer-c_050c0806-zone-data_center'
+        password = '/ykw+71y9e~o'
+        ip = self.get_random_ip()
+        auth = HTTPProxyAuth("%s-ip-%s" % (username, ip), password)
+        return auth
 
-    @retry((JSONDecodeError, requests.exceptions.ProxyError, ReadTimeout, MaxRetryError), tries=12, delay=0.25)
+    def get_random_ip(self):
+        global ips
+        if not ips or ips == []:
+            ips = get_ips()
+        ip = random.choice(ips)
+        return ip['ip']
+
+    @retry((JSONDecodeError, requests.exceptions.ProxyError, ReadTimeout, MaxRetryError, SSLError, URLError,
+            NewConnectionError), tries=12,
+           delay=0.25)
     def fetch_data(self, data=None) -> dict:
         """
         Fetching data by using requests.get
@@ -35,17 +67,31 @@ class SiteCollector(TickerCollector, ABC):
         """
         if not data:
             url = self.site.get_ticker_url(self.ticker)
-            response = requests.get(url, timeout=2, proxies=Runnable.proxy)
+            session = requests.Session()
+            session.auth = self.get_proxy_auth()
+
+            session.proxies = {"http": proxy, "https": proxy}
+            response = session.get(url, timeout=5)
 
             if response.status_code == 404:
                 logger.warning("Non existing ticker: {ticker}: {url} -> 404 error code".format(
                     ticker=self.ticker, url=url))
                 raise InvalidTickerExcpetion(self.ticker)
 
+            if response.status_code == 429:
+                logger.warning("Can't collect {ticker}: {url} -> responsne code: {code}".format(
+                    ticker=self.ticker, url=url, code=response.status_code))
+                raise requests.exceptions.ProxyError()
+
+            if 500 <= response.status_code <= 599:
+                logger.warning("Can't collect {ticker}: {url} -> responsne code: {code}".format(
+                    ticker=self.ticker, url=url, code=response.status_code))
+                raise ReadTimeout()
+
             if response.status_code != 200:
                 logger.warning("Can't collect {ticker}: {url} -> responsne code: {code}".format(
                     ticker=self.ticker, url=url, code=response.status_code))
-                raise InvalidTickerExcpetion(self.ticker)
+                raise InvalidTickerExcpetion(self.ticker, response.status_code)
 
             data = response.json()
 
