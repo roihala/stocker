7755dbb7797118069ad484967c1adc65c801db50
itayshmuel <2000itay@gmail.com>
2021-08-06 16:18:15 +0300

    fixed alert relevant_tier bug

--- alert.py
+++ alert.py
@@ -135,9 +135,16 @@ class Alert(CommonRunnable):
 
     def is_relevant(self, ticker, price):
         try:
-            tier = readers.Securities(self._mongo_db, ticker).get_latest().get('tierDisplayName')
+            sec = readers.Securities(self._mongo_db, ticker).get_latest()
+            tier = sec.get('tierDisplayName')
+            tier_code = sec.get('tierCode')
+
             tier_hierarchy = Securities.get_hierarchy()['tierDisplayName']
             relevant_tier = tier_hierarchy.index(tier) < tier_hierarchy.index('OTCQB')
+
+            if not relevant_tier:
+                tier_hierarchy = list(Securities.get_tier_translation().keys())
+                relevant_tier = tier_hierarchy.index(tier) > tier_hierarchy.index('OT')
 
         except (ValueError, AttributeError):
             relevant_tier = True
