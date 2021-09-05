bc33f07cefeb430f4a0e00e7bc084303f14e0ffb
Roi Halamish <roihala@hotmail.com>
2021-09-05 17:59:33 +0300

    Added headers

--- src/collect/tickers/site_collector.py
+++ src/collect/tickers/site_collector.py
@@ -38,6 +38,22 @@ def get_ips(is_debug):
 
 
 class SiteCollector(TickerCollector, ABC):
+    REQUIRED_HEADERS = {
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Host': 'backend.otcmarkets.com',
+        'Origin': 'https://www.otcmarkets.com',
+        'Referer': 'https://www.otcmarkets.com/',
+        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        'sec-ch-ua-mobile': '?0',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+    }
+
     @property
     @abstractmethod
     def site(self):
@@ -75,7 +91,7 @@ class SiteCollector(TickerCollector, ABC):
             url = self.site.get_ticker_url(self.ticker)
 
             if self._debug:
-                response = requests.get(url)
+                response = requests.get(url, headers=self.REQUIRED_HEADERS)
                 if response.status_code == 200:
                     return response.json()
                 else:
