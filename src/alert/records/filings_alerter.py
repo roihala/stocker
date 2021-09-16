2d3d5f17cd97000439841b0bc16a980f1c07ad8c
Roi Halamish <roihala@hotmail.com>
2021-09-16 20:32:21 +0300

    Fixed filingsAlerter

--- src/alert/records/filings_alerter.py
+++ src/alert/records/filings_alerter.py
@@ -30,7 +30,7 @@ class FilingsAlerter(AlerterBase, ABC):
     @property
     def site(self) -> Site:
         return Site('filings',
-                    'https://backend.otcmarkets.com/otcapi/company/{ticker}/financial-report?symbol={'
+                    'http://backend.otcmarkets.com/otcapi/company/{ticker}/financial-report?symbol={'
                     'ticker}&page=1&pageSize=50&statusId=A&sortOn=releaseDate&sortDir=DESC',
                     True)
 
